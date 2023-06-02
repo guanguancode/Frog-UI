6222a14f309141cae6d369ed1aef35044fbfc24b
guanguancode <1271835875@qq.com>
2023-06-02 10:04:39 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>hi</div>
+  <self />
+</template>
+
+<script>
+import self from "./components/self.vue";
+
+export default {
+  name: "App",
+  components: {
+    self: self,
+  },
+};
+</script>
---
+++ src/components/self.vue
@@ -0,0 +1,3 @@
+<template>
+  <div>自己的第一个组件</div>
+</template>
