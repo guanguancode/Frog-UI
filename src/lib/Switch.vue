17c1819684c588e31e79a5790ba5ec9add0ceb3c
guanguancode <1271835875@qq.com>
2023-06-04 17:35:07 +0800

    代码优化

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -11,7 +11,6 @@ export default {
     value: Boolean,
   },
   setup(props, context) {
-    const checked = ref(false);
     const toggle = () => {
       context.emit("update:value", !props.value);
     };
@@ -30,35 +29,35 @@ button {
   background: #bfbfbf;
   border-radius: $h/2;
   position: relative;
-}
-span {
-  position: absolute;
-  left: 2px;
-  top: 2px;
-  height: $h2;
-  width: $h2;
-  background: white;
-  border-radius: $h2 / 2;
-  transition: all 250ms;
-}
-button.checked {
-  background: #1890ff;
-}
-button.checked > span {
-  left: calc(100% - #{$h2} - 2px);
-}
-button:focus {
-  outline: none;
-}
-button:active {
   > span {
-    width: $h2 + 4px;
+    position: absolute;
+    left: 2px;
+    top: 2px;
+    height: $h2;
+    width: $h2;
+    background: white;
+    border-radius: $h2 / 2;
+    transition: all 250ms;
   }
-}
-button.checked:active {
-  > span {
-    width: $h2 + 4px;
-    margin-left: -4px;
+  &.checked {
+    background: #1890ff;
+    > span {
+      left: calc(100% - #{$h2} - 2px);
+    }
+  }
+  &:focus {
+    outline: none;
+  }
+  &:active {
+    > span {
+      width: $h2 + 4px;
+    }
+  }
+  &.checked:active {
+    > span {
+      width: $h2 + 4px;
+      margin-left: -4px;
+    }
   }
 }
 </style>
