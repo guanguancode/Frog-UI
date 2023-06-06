988356c7dc910de5bafbfad71e0f04653daeffca
guanguancode <1271835875@qq.com>
2023-06-06 22:47:27 +0800

    创建 Dialog 组件

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,3 +1,14 @@
 <template>
   <div>Dialog 组件</div>
+  <h1>示例1</h1>
+  <Dialog></Dialog>
 </template>
+
+<script lang="ts">
+import Dialog from "../lib/Dialog.vue";
+export default {
+  components: {
+    Dialog,
+  },
+};
+</script>
