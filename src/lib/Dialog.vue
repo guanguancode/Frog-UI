b042dea37e900fe3ff1b15eb304eb3f27509a08f
guanguancode <1271835875@qq.com>
2023-06-06 23:10:17 +0800

    Dialog 支持 visible

--- src/lib/Dialog.vue
+++ src/lib/Dialog.vue
@@ -1,23 +1,34 @@
 <template>
-  <div class="frog-dialog-overlay"></div>
-  <div class="frog-dialog-wrapper">
-    <div class="frog-dialog">
-      <header>标题 <span class="frog-dialog-close"></span></header>
-      <main>
-        <p>第一行字</p>
-        <p>第二行字</p>
-      </main>
-      <footer>
-        <Button level="main">OK</Button>
-        <Button>Cancel</Button>
-      </footer>
+  <template v-if="visible">
+    <div class="frog-dialog-overlay"></div>
+    <div class="frog-dialog-wrapper">
+      <div class="frog-dialog">
+        <header>
+          标题
+          <span class="frog-dialog-close"></span>
+        </header>
+        <main>
+          <p>第一行字</p>
+          <p>第二行字</p>
+        </main>
+        <footer>
+          <Button level="main">OK</Button>
+          <Button>Cancel</Button>
+        </footer>
+      </div>
     </div>
-  </div>
+  </template>
 </template>
 
 <script lang="ts">
 import Button from "./Button.vue";
 export default {
+  props: {
+    visible: {
+      type: Boolean,
+      default: false,
+    },
+  },
   components: {
     Button,
   },
