25671c367216bcd84b5b02db73ffc745c3902494
guanguancode <1271835875@qq.com>
2023-06-07 18:27:33 +0800

    Tabs css 1

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,6 +1,21 @@
 <template>
-  <div>
-    <div v-for="(t, index) in titles" :key="index">{{ t }}</div>
+  <div class="frog-tabs">
+    <div class="frog-tabs-nav">
+      <div
+        class="frog-tabs-nav-item selected"
+        v-for="(t, index) in titles"
+        :key="index"
+      >
+        {{ t }}
+      </div>
+    </div>
+    <div class="frog-tabs-content">
+      <component
+        class="frog-tabs-content-item"
+        v-for="(c, index) in defaults"
+        :is="c"
+      ></component>
+    </div>
   </div>
 </template>
 
@@ -24,3 +39,30 @@ export default {
   },
 };
 </script>
+
+<style lang="scss">
+$blue: #40a9ff;
+$color: #333;
+$border-color: #d9d9d9;
+.frog-tabs {
+  &-nav {
+    display: flex;
+    color: $color;
+    border-bottom: 1px solid $border-color;
+    &-item {
+      padding: 8px 0;
+      margin: 0 16px;
+      cursor: pointer;
+      &:first-child {
+        margin-left: 0;
+      }
+      &.selected {
+        color: $blue;
+      }
+    }
+  }
+  &-content {
+    padding: 8px 0;
+  }
+}
+</style>
