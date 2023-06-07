cb3960c508277c9733d7ce20667ab09871f5eb77
guanguancode <1271835875@qq.com>
2023-06-07 11:05:47 +0800

    Dialog 支持点击关闭

--- src/lib/Dialog.vue
+++ src/lib/Dialog.vue
@@ -1,19 +1,19 @@
 <template>
   <template v-if="visible">
-    <div class="frog-dialog-overlay"></div>
+    <div class="frog-dialog-overlay" @click="onClickOverlay"></div>
     <div class="frog-dialog-wrapper">
       <div class="frog-dialog">
         <header>
           标题
-          <span class="frog-dialog-close"></span>
+          <span @click="close" class="frog-dialog-close"></span>
         </header>
         <main>
           <p>第一行字</p>
           <p>第二行字</p>
         </main>
         <footer>
-          <Button level="main">OK</Button>
-          <Button>Cancel</Button>
+          <Button @click="ok" level="main">OK</Button>
+          <Button @click="cancel">Cancel</Button>
         </footer>
       </div>
     </div>
@@ -28,9 +28,44 @@ export default {
       type: Boolean,
       default: false,
     },
+    closeOnClickOverlay: {
+      type: Boolean,
+      default: true,
+    },
+    ok: {
+      type: Function,
+    },
+    cancel: {
+      type: Function,
+    },
   },
   components: {
     Button,
+  },
+  setup(props, context) {
+    const close = () => {
+      context.emit("update:visible", false);
+    };
+    const onClickOverlay = () => {
+      if (props.closeOnClickOverlay) {
+        close();
+      }
+    };
+    const ok = () => {
+      if (props.ok?.() !== false) {
+        close();
+      }
+    };
+    const cancel = () => {
+      context.emit("cancel");
+      close();
+    };
+    return {
+      close,
+      onClickOverlay,
+      ok,
+      cancel,
+    };
   },
 };
 </script>
