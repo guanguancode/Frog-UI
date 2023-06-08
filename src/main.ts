4b7309b1272daa0948f03f826bb8907cbfff7b73
guanguancode <1271835875@qq.com>
2023-06-08 20:25:24 +0800

    完善 markdown 文件

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import "./lib/frog.scss";
 import "./index.scss";
 import "./lib/svg.js";
+import "github-markdown-css";
 import { createApp } from "vue";
 import App from "./App.vue";
 import { router } from "./router";
