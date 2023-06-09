c5cc7b9a3c89e8361362824b4fd28b8c90e0e3f7
guanguancode <1271835875@qq.com>
2023-06-09 12:36:15 +0800

    优化了SwitchDemo

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -4,7 +4,7 @@
     <div class="demo">
       <h2>常规用法</h2>
       <div class="demo-component">
-        <Switch1Demo />
+        <component :is="Switch1Demo" />
       </div>
       <div class="demo-actions"></div>
       <Button>查看代码</Button>
@@ -16,7 +16,7 @@
   <div class="demo">
     <h2>支持 disabled</h2>
     <div class="demo-component">
-      <Switch2Demo />
+      <component :is="Switch2Demo" />
     </div>
     <div class="demo-actions">
       <Button>查看代码</Button>
@@ -32,10 +32,9 @@ import Switch from "../lib/Switch.vue";
 import { ref } from "vue";
 import Button from "../lib/Button.vue";
 import Switch1Demo from "./Switch1.demo.vue";
-console.log(Switch1Demo.__sourceCode);
 import Switch2Demo from "./Switch2.demo.vue";
 export default {
-  components: { Switch1Demo, Switch2Demo, Switch, Button },
+  components: { Button },
   setup() {
     const bool = ref(false);
     return { bool, Switch1Demo, Switch2Demo };
